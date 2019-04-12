270e101010f7335ecdcad1c49179bb6b294589e8
Markus Pettersson <markus.pettersson1998@gmail.com>
2019-04-12 15:35:04 +0200

    Created BlockShopView component
    Refactored code to increase readabillity. Functionally the same as before.

---
+++ App/pages/components/BlockShopView.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import {View, ImageBackground, Text, StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+    innerC: {
+        width: '100%',
+        flexDirection: 'row',
+        backgroundColor: '#F0F7F4',
+        alignItems: 'flex-start',
+    },
+    item: {
+        height: 200,
+        width: '50%',
+        borderWidth: 10,
+        justifyContent: 'center',
+        borderColor: '#F0F7F4',
+        backgroundColor: 'black'
+    },
+    text: {
+        fontWeight: 'bold',
+        fontSize: 25,
+        color: 'white',
+    }
+});
+
+
+const BlockShopView = ({index, name, picture}) => {
+    return (
+        <View style = {styles.innerC}>
+            <View style = {styles.item}>
+                <ImageBackground key = {index} source = {picture} resizeMode = 'cover' style={{width: '100%', height: '100%', borderRadius: 20}}  >
+                    <Text style={styles.text}>{name}</Text>
+                </ImageBackground>
+            </View>
+        </View>
+    );
+};
+
+export default BlockShopView;
