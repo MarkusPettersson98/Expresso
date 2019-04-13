5080b53230b3ed2ed4d8df52aa2bfc95439f24d7
RobertZetterlund <31474146+RobertZetterlund@users.noreply.github.com>
2019-04-13 18:25:09 +0200

    added coordinates to dummydata, added dynamic markers to map

--- App/pages/components/dummy-data.js
+++ App/pages/components/dummy-data.js
@@ -1,19 +1,43 @@
 const shops = [
     {
-        "name": "Biblioteket",
-        "picture": require('./resources/biblan.jpg'),
+        name: "Biblioteket",
+        picture: require("./resources/biblan.jpg"),
+        coordinates : {
+            latitude: 57.690382,
+            longitude: 11.978556,
+        },
     },
     {
-        "name": "Bulten",
-        "picture": require('./resources/bulten.jpg'),
+        name: "Bulten",
+        picture: require("./resources/bulten.jpg"),
+        coordinates: {
+            latitude: 57.689008,
+            longitude: 11.978538,
+        },
     },
     {
-        "name": "Veras Café",
-        "picture": require('./resources/vera.jpg'),
+        name: "Linsen",
+        picture: require("./resources/linsen.jpg"),
+        coordinates : {
+            latitude: 57.687962,
+            longitude: 11.978813,
+        },
     },
     {
-        "name": "Wijkanders",
-        "picture": require('./resources/wijkanders.jpg'),
+        name: "Veras Café",
+        picture: require("./resources/vera.jpg"),
+        coordinates: {
+            latitude: 57.693158,
+            longitude: 11.975036,
+        }
+    },
+    {
+        name: "Wijkanders",
+        picture: require("./resources/wijkanders.jpg"),
+        coordinates : {
+            latitude: 57.692538,
+            longitude: 11.975390,
+        },
     },
 ];
 
