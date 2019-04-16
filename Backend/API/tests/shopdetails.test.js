314b5d3fa1edfcc16e956a0fc78a520a6c2ee46a
Markus Pettersson <markus.pettersson1998@gmail.com>
2019-04-16 10:50:31 +0200

    Added more testcases

--- Backend/API/tests/shopdetails.test.js
+++ Backend/API/tests/shopdetails.test.js
@@ -7,9 +7,21 @@ test('Get the requested shop back', () => {
     expect(foundShop).toBe(shop);
 });
 
+test('Indicate that shop does not exists if requesting unlisted shop', () => {
+    const shop = 'Bulten234';
+    const foundShop = api.lookUpShop(shop);
+    expect(foundShop).toBe(false);
+});
+
 test('Get the correct coffee assortment from a shop', () => {
     const shop = 'Bulten';
     const shopAssortment = testData.assortment.find(assortment => assortment.shop === shop).coffees;
     const foundCoffee = api.lookUpCoffee(shop);
     expect(foundCoffee).toBe(shopAssortment);
+});
+
+test('Indicate that coffee assortment from unlisted shop does not exist', () => {
+    const shop = 'Bulten123';
+    const foundCoffee = api.lookUpCoffee(shop);
+    expect(foundCoffee).toBe(false);
 });
