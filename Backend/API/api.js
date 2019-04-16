843f06833ee2b00d9db8fa91abb7862061989748
Markus Pettersson <markus.pettersson1998@gmail.com>
2019-04-16 09:26:05 +0200

    Backend/dev api (#10)
    * la till databas i backend
    
    * Created api for shops and coffee assortment
    Created endpoint in api for getting:
    1. All shops
    2. One shop
    3. All coffee products from one shop. 
    
    If the place you queried does not exists, then you get a http response
    with error code 400 back. ☕️

---
+++ Backend/API/api.js
@@ -0,0 +1,61 @@
+/* Function definitions for handling API requests */
+
+const { shops, coffee } = require("../Database/data.js");
+
+const getAllShops = (req, res) => {
+    /*
+      Return status code 200 (request was OK) and
+      write all shops to response header
+    */
+    return res.status(200).send(shops);
+};
+
+const getShop = (req, res) => {
+    return detailsFromShop(lookUpShop, req, res);
+};
+
+const getCoffee = (req, res) => {
+    return detailsFromShop(lookUpCoffee, req, res);
+};
+
+const lookUpShop = (requestedShop) => {
+    /*
+      Return all available information about requested shop
+    */
+    const lookup = shop => shop.name === requestedShop;
+    return shops.find(lookup);
+};
+
+const lookUpCoffee = (requestedShop) => {
+    /*
+      Return all coffee products from requested shop
+    */
+    const lookup = location => location.shop === requestedShop;
+    const foundShop = coffee.find(lookup);
+    return foundShop.coffees;
+};
+
+const detailsFromShop = (lookUp, req, res) => {
+    /*
+      Wrapper function to deal with return of status code. Details of
+      what data to return is decided by param lookUp.
+    */
+
+    // Try to parse shop name from request
+    const shop = req.params.shop;
+    if (shop === undefined) res.status(400).end();
+
+    const details = lookUp(shop);
+
+    // Shop was found, return OK
+    if (details) return res.status(200).send(details);
+
+    // Shop was not found, return error
+    return res.status(400).end();
+};
+
+module.exports = {
+    getAllShops: getAllShops,
+    getShop: getShop,
+    getCoffee: getCoffee
+};
