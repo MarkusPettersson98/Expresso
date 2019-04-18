40fa5a0d78258dad31df3d4c9600bf40dc2929da
RobertZetterlund <31474146+RobertZetterlund@users.noreply.github.com>
2019-04-18 12:56:15 +0200

    improvement: made clearCartIcon render depend on cartState

--- App/pages/components/header/ClearCheckoutIcon.js
+++ App/pages/components/header/ClearCheckoutIcon.js
@@ -4,20 +4,29 @@ import { Feather } from '@expo/vector-icons';
 import { connect } from 'react-redux';
 import { clearCart } from '../redux/actions';
 
-const ClearCheckout = ({ dispatch }) => {
+const ClearCheckout = ( {cart, dispatch} ) => {
+    // checks if the cart is using wacky logic, could/should be swapped with prettier solution
+    // renders conditionally
+    let emptyCart = !Object.keys(cart).length;
     return (
         <View style={{ flexDirection: 'row', margin: 5 }}>
             <TouchableOpacity
                 onPress={() => dispatch(clearCart())}
+                disabled={emptyCart}
             >
                 <Feather
                     name="trash-2"
                     size={24}
-                    color="#fff"
+                    color={emptyCart ? '#9A8F93' : '#fff'}
                 />
             </TouchableOpacity>
         </View>
     );
 };
 
-export default connect()(ClearCheckout);
+const mapStatesToProps = state => {
+    return { cart: state.cart };
+};
+
+export default connect(mapStatesToProps)(ClearCheckout);
+
