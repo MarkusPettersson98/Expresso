8c4c1a417593866ffb27765212d215c0478bcde7
RobertZetterlund <31474146+RobertZetterlund@users.noreply.github.com>
2019-04-20 13:45:39 +0200

    added scrolling of items to the checkoutview

--- App/pages/Checkout.js
+++ App/pages/Checkout.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, TouchableOpacity } from 'react-native';
+import { View, ScrollView, TouchableOpacity } from 'react-native';
 import { brygg_kaffe, cappuccino } from './components/dummy-data';
 import { connect } from 'react-redux';
 import { AntDesign } from '@expo/vector-icons';
@@ -12,7 +12,7 @@ import TotalAmount from './components/checkout/TotalAmount';
 const CheckoutPage = props => {
     let isCartPopulated = Object.keys(props.cart).length;
     let total = 0;
-    Object.values(props.cart).forEach((orderItem) => {
+    Object.values(props.cart).forEach(orderItem => {
         total = total + orderItem.coffee.price * orderItem.amount;
     });
 
@@ -44,10 +44,20 @@ const CheckoutPage = props => {
                     alignItems: 'center',
                 }}
             >
-                {!isCartPopulated && <EmptycheckoutPage />}
-                {Object.values(props.cart).map((orderItem, i) => (
-                    <CheckoutItem key={i} orderItem={orderItem} />
-                ))}
+                
+                <ScrollView
+                        contentContainerStyle={{
+                            flex: 10,
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            width: '100%',
+                        }}
+                    >
+                        {Object.values(props.cart).map((orderItem, i) => (
+                            <CheckoutItem key={i} orderItem={orderItem} />
+                        ))}
+                    </ScrollView>
+                
             </View>
             {!isCartPopulated || <TotalAmount total={total} />}
             <View
@@ -61,14 +71,14 @@ const CheckoutPage = props => {
                     <OrderButton
                         onPress={() => {
                             console.log('Du beställde:');
-                            Object.values(props.cart).forEach((orderItem) => {
+                            Object.values(props.cart).forEach(orderItem => {
                                 console.log(
                                     orderItem.amount,
                                     ':',
                                     orderItem.coffee.name,
                                 );
                             });
-                            console.log('Kostnad:', total)
+                            console.log('Kostnad:', total);
                         }}
                     />
                 )}
