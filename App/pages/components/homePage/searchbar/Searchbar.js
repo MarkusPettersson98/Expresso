a79cb75a0d66ad2c10e6e49e36bf71aa018d2463
Markus Pettersson <markus.pettersson1998@gmail.com>
2019-04-21 10:30:22 +0200

    Added basic styling to searchbar Absolutely not final

--- App/pages/components/homePage/searchbar/Searchbar.js
+++ App/pages/components/homePage/searchbar/Searchbar.js
@@ -9,11 +9,25 @@ const Searchbar = ({ searchFilter, onChange }) => {
                 const applySearchFilter = (collection) => searchFilter(expression, collection);
                 onChange(applySearchFilter);
             }}
-            placeholder='Placeholder'
+            placeholder='Sök café ..'
+            style={styles.searchText}
             clearButtonMode={'while-editing'}
         />
     );
 };
+
+const styles = StyleSheet.create({
+    searchText: {
+        fontSize: 26,
+        fontWeight: 'normal',
+        color: '#57454B',
+        paddingTop: 2,
+        paddingBottom: 2,
+        paddingLeft: 0,
+        paddingRight: 0,
+        borderWidth: 1,
+    }
+});
 
 
 
