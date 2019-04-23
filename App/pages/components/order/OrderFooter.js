b972e89f9b6e7c7d36db3262447e3adc1328485b
Markus Pettersson <markus.pettersson1998@gmail.com>
2019-04-23 11:18:14 +0200

    Added styling and OrderCartIcon to OrderFooter

--- App/pages/components/order/OrderFooter.js
+++ App/pages/components/order/OrderFooter.js
@@ -1,10 +1,40 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, TouchableOpacity, StyleSheet } from 'react-native';
 
-const OrderFooter = () => {
+import OrderCartIcon from './OrderCartIcon';
+
+const OrderFooter = (props) => {
     return (
-        <View></View>
+        <View style={styles.footer}>
+            <OrderCartIcon cartAmount={10} />
+            <TouchableOpacity
+                onPress={() => props.navigationProps.navigate('Checkout')}
+                hitSlop={{ top: 10, bottom: 10, left: 10, right: 10 }}
+            >
+            </TouchableOpacity>
+        </View>
     );
-}
+};
+
+const styles = StyleSheet.create({
+    footer: {
+        flex: 1,
+        justifyContent: 'flex-end',
+        marginBottom: 40,
+        backgroundColor: 'brown',
+        maxHeight: 80,
+    },
+    cartIcon: {
+        height: 50,
+        justifyContent: 'center',
+        borderColor: '#F0F7F4',
+        backgroundColor: 'black',
+    },
+    text: {
+        fontWeight: 'bold',
+        fontSize: 25,
+        color: 'white',
+    },
+});
 
 export default OrderFooter;
