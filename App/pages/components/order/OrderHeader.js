28b7ab50707e4bf70621088d47b79eb635ff6dfb
Markus Pettersson <markus.pettersson1998@gmail.com>
2019-04-23 11:15:20 +0200

    Added flex to OrderHeader

--- App/pages/components/order/OrderHeader.js
+++ App/pages/components/order/OrderHeader.js
@@ -16,6 +16,7 @@ const OrderHeader = ({ picture }) => {
 
 const styles = StyleSheet.create({
     item: {
+        justifyContent: 'flex-start',
         height: 250,
         justifyContent: 'center',
         borderColor: '#F0F7F4',
