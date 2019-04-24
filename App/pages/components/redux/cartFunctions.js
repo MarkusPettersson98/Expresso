36a03fb6eba90833d2534e4901e5686222f30fff
Markus Pettersson <markus.pettersson1998@gmail.com>
2019-04-24 12:05:01 +0200

    Refactoring: Rewrote functions calculating cart stuff. Defensive programming be gone, long rule Arrays.redue

--- App/pages/components/redux/cartFunctions.js
+++ App/pages/components/redux/cartFunctions.js
@@ -1,13 +1,26 @@
 export const calculateCartPrice = orderItems => {
-    let total = 0;
-    // check if the orderItem is an empty object using
-    // https://coderwall.com/p/_g3x9q/how-to-check-if-javascript-object-is-empty
-    if (Object.keys(orderItems).length) {
-        // loop through object using https://stackoverflow.com/a/5737192
-        console.log(Object.keys(orderItems));
-        Object.keys(orderItems).forEach(function(key) {
-            total = total + orderItems[key].coffee.price * orderItems[key].amount;
-        });
-    }
-    return total;
+    // Iterate over all keys (coffeeids). For every unique coffeeid, calculate total cost for that coffee
+    // variant and add to total cost
+
+    const totalOrderItemPrice = orderItem => orderItem.coffee.price * orderItem.amount;
+
+    return Object.keys(orderItems).reduce(
+        (currentTotalPrice, key) => {
+            const orderItem = orderItems[key];
+            return currentTotalPrice + totalOrderItemPrice(orderItem);
+        }, 0);
+
+};
+
+export const calculateCartAmount = orderItems => {
+    // Iterate over all keys (coffeeids). For every unique coffeeid, check how many coffees
+    // there are in cart and add to total amount
+
+    const totalOrderItemAmount = orderItem => orderItem.amount;
+    
+    return Object.keys(orderItems).reduce(
+        ((currentTotalAmount, key) =>  {
+            const orderItem = orderItems[key];
+            return currentTotalAmount + totalOrderItemAmount(orderItem);
+        }), 0);
 };
