5466219bda53e3a10df5b8a9226b51efa5055039
lucasekmarkfallqvist <lucasekmarkfallqvist@gmail.com>
2019-04-26 14:13:25 +0200

    La till alla nuvarande API funktioner som används i Backend.
    Fixade så att allt är case-insensitive och strukturerade om en del i dummy-data. Vi förväntar oss t.ex. nu att en shop har en drinkList som skriver ut alla kaffestorter som de har inne.
    Anledningen till att MainComp är ändrad är bara för att jag testade via denna fil.

--- App/pages/components/homePage/Maincomp.js
+++ App/pages/components/homePage/Maincomp.js
@@ -4,16 +4,10 @@ import { AppRegistry, ScrollView } from 'react-native';
 import { default as ShopView } from './ListShopView';
 import shops from '../dummy-data';
 
-import {getAllShopNames, getAllShopInfo, getCoffeeInfo, getAllCoffeeFromAShop} from '../../../API/expressoAPI';
-import { brewed_coffee, cappuccino } from '../../../API/dummy-data';
-
 export default (Maincomp = props => {
     const ShopViews = shops.map((shop, index) => {
         return <ShopView key={index} name={shop.name} picture={shop.picture} />;
     });
-
-    console.log( getAllCoffeeFromAShop('Bulten')); 
-
 
     return (
         <ScrollView contentContainerStyle={styles.container}>
