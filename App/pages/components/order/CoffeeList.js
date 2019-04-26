84b37b9ea77682b562e7fec8aa719398d3f45df9
Markus Pettersson <markus.pettersson1998@gmail.com>
2019-04-26 17:20:44 +0200

    Coffeelist

--- App/pages/components/order/CoffeeList.js
+++ App/pages/components/order/CoffeeList.js
@@ -4,6 +4,17 @@ import { AppRegistry, ScrollView, StyleSheet } from 'react-native';
 import { default as CoffeeItem } from './CoffeeItem';
 import { coffee } from '../dummy-data';
 
+/**
+ * @file This is the list responsible for rendering all different coffees from a shop.
+ *
+ * @param selectedShop Name of the selected shop. Used to fetch coffee assortment
+ *
+ * @todo CoffeeList should be passed down all the necessary information from Order. It's
+ * only job should be to render the objects it gets passed down and nothing more. This means
+ * that rather than receiving @param selectedShop it should be passed a list of coffeeobjects.
+ *
+ */
+
 const styles = StyleSheet.create({
     container: {
         width: '100%',
