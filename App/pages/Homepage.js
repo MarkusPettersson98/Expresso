f5772972bc2e40b1512bf02b55f984d6b1208601
Robert <31474146+RobertZetterlund@users.noreply.github.com>
2019-04-30 15:21:00 +0200

    changed background color of container, improved tab-styling (#43)

--- App/pages/Homepage.js
+++ App/pages/Homepage.js
@@ -26,6 +26,7 @@ export default class Homepage extends Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        backgroundColor: '#F0F7F4',
     },
     comps: {
         flex: 1,
