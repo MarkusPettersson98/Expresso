fb2efc369f9f239a88ef3e2385659d7c772cce86
lucasekmarkfallqvist <lucasekmarkfallqvist@gmail.com>
2019-05-01 17:35:56 +0200

    La till en getShopPicture API och refaktorerade och indenterade
    Refaktorerade ut findShop så att man kan återanvända funktionaliteten för att hitta en shop till andra API
    Installerade prettier och indenterade om alla dokument

--- App/API/expressoAPI.js
+++ App/API/expressoAPI.js
@@ -1,66 +1,73 @@
 import allData from './dummy-data';
-
-
+import defaultPic from '../pages/components/resources/ExpressoTransp.png';
 /**
-  * Returns all the names of all the shops in the database
-  * This only inlcudes the names of the shops  
-  */
+ * Returns all the names of all the shops in the database
+ * This only inlcudes the names of the shops
+ */
 export const getAllShopNames = () => {
-
-  const getName = (shop) => {
-    return shop.name
-  }
-
-  return allData.shops.map(getName);
+    const getName = shop => {
+        return shop.name;
+    };
+    return allData.shops.map(getName);
 };
 
 /**
-  * Returns all the information of a all the shops in the database
-  * This includes the names and the coordinates  
-  */
+ * Returns all the information of a all the shops in the database
+ * This includes the names and the coordinates
+ */
 export const getAllShopInfo = () => {
+    const getInformation = info => {
+        return {
+            namn: info.name,
+            coordinates: info.coordinates,
+        };
+    };
+    return allData.shops.map(getInformation);
+};
 
-  const getInformation = (info) => {
+/**
+ * Returns all the coffesorts in a specific shop
+ * @param wantedShop  The name of the wanted shop
+ */
+export const getAllCoffeeFromAShop = wantedShop => {
+    return findShop(wantedShop).drinkList;
+};
+
+/**
+ * Returns all the information about a specific shop
+ * This includes name, coordinates and the drinkList of the shop
+ * @param wantedShop  The name of the wanted shop
+ */
+export const getShop = wantedShop => {
+    const foundShop = findShop(wantedShop); 
     return {
-      namn: info.name,
-      coordinates: info.coordinates
-    }
-  }
-  return allData.shops.map(getInformation);
+        name: foundShop.name,
+        coordinates: foundShop.coordinates,
+        drinkList: foundShop.drinkList,
+    };
 };
 
 /**
-  * Returns all the coffesorts in a specific shop 
-  * @param wantedShop  The name of the wanted shop 
-  */
-export const getAllCoffeeFromAShop = (wantedShop) => {
-
-
-  const foundShop = allData.shops.find(function (shop) {
-    return shop.name.toUpperCase() == wantedShop.toUpperCase();
-  });
-
-  return foundShop.drinkList;
+ * Returns all the information about a specific shop
+ * This only includes the picture of the requested shop
+ * @param wantedShop  The name of the wanted shop
+ */
+export const getShopPicture = wantedShop => {
+    const foundShop = findShop(wantedShop); 
+    return foundShop.picture
+        ? foundShop.picture
+        : defaultPic;
 };
 
 /**
-  * Returns all the information about a specific shop 
-  * This includes name, coordinates and the drinkList of the shop 
-  * @param wantedShop  The name of the wanted shop 
-  */
-export const getShop = (wantedShop) => {
+ * Returns a shop
+ * This is the shop object itself
+ * @param wantedShop  The name of the wanted shop
+ */
+export const findShop = wantedShop => {
+    const foundShop = allData.shops.find(function(shop) {
+        return shop.name.toUpperCase() == wantedShop.toUpperCase();
+    });
 
-  const foundShop = allData.shops.find(function (shop) {
-
-
-    return shop.name.toUpperCase() == wantedShop.toUpperCase();
-  }
-
-  );
-
-   return {
-    name: foundShop.name,
-    coordinates: foundShop.coordinates,
-    drinkList: foundShop.drinkList,
-  };
+    return foundShop;
 };
