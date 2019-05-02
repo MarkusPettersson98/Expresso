0eb6f01850fb5882e2c2261e975498483a071d21
Markus Pettersson <markus.pettersson1998@gmail.com>
2019-05-02 18:03:08 +0200

    Refactored Maincomp to use async API

--- App/pages/components/homePage/Maincomp.js
+++ App/pages/components/homePage/Maincomp.js
@@ -2,19 +2,48 @@ import React from 'react';
 import { AppRegistry, ScrollView } from 'react-native';
 
 import { default as ShopView } from './ListShopView';
-import shops from '../dummy-data';
+import { getAllShopNames, getShopPicture } from '../../../API/expressoAPI';
 
-export default (Maincomp = props => {
-    const ShopViews = shops.map((shop, index) => {
-        return <ShopView key={index} name={shop.name} picture={shop.picture} />;
-    });
+export default class Maincomp extends React.Component {
 
-    return (
-        <ScrollView contentContainerStyle={styles.container}>
-            {ShopViews}
-        </ScrollView>
-    );
-});
+    constructor(props) {
+        super(props);
+        this.state = { ShopViews: [] };
+    };
+
+    async componentDidMount() {
+        // Request all shops names
+        const shopNames = await getAllShopNames();
+
+        const shopsWithPictures = await shopNames.map(async shopName => {
+            return {
+                name: shopName,
+                picture: await getShopPicture(shopName),
+            };
+        });
+
+        // When all promises have resolved, update state
+        Promise.all(shopsWithPictures).then(shops => {
+            // Create ShopView components to be rendered
+            const shopViews = shops.map((shop, index) => (
+                <ShopView key={index} name={shop.name} picture={shop.picture} />
+            ));
+
+            // Update state
+            this.setState({
+                ShopViews: shopViews,
+            });
+        });
+    };
+
+    render() {
+        return (
+            <ScrollView contentContainerStyle={styles.container}>
+                {this.state.ShopViews}
+            </ScrollView>
+        );
+    }
+}
 
 const styles = {
     container: {
