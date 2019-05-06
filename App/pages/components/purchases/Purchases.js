aa1de4b6f70d42f01d566f549e0b5d915f029827
Emil Svensson <esvensson1997@gmail.com>
2019-05-06 18:34:12 +0200

    QR kod och enkel layout
    En enkel layout för att displaya QR kod och kvitto från ett köp i 'Köp'.

--- App/pages/components/purchases/Purchases.js
+++ App/pages/components/purchases/Purchases.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react';
-import {
-    View,
-    Text,
-    StyleSheet,
-    TouchableOpacity,
-} from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { connect } from 'react-redux';
+import QRCode from 'react-native-qrcode';
+
+/**
+ * Här ser jag att vi 'kör igång' vår page och hämtar in objektet från backend för att sedan styra komponenterna.
+ * I props tar vi in userID genom mapStateToProps och skickar sedan vidare uniqueID för att få en QR kod och
+ * rätt attribut som ska visas som information om köpet för användaren.
+ */
 
 const Purchases = props => {
-    return(
-        <View style = {styles.container}>
-            <Text>This will contain the QR code and other fun stuff</Text>
+    return (
+        <View style={styles.container}>
+            <View style={styles.innerContainer}>
+                <View style={styles.qr}>
+                    <QRCode
+                        value={'This is the QR-value'}
+                        size={260}
+                        bgColor="black"
+                        fgColor="white"
+                    />
+                </View>
+            </View>
+            <View style={styles.tc}>
+                <Text style={styles.text}>Information about pur.</Text>
+            </View>
         </View>
     );
 };
@@ -18,9 +32,37 @@ const Purchases = props => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    innerContainer: {
         justifyContent: 'center',
-        alignContent: 'center'
+        alignItems: 'center',
+        height: '70%',
+        width: '100%',
+    },
+    qr: {
+        width: 300,
+        height: 300,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderWidth: 5,
+        borderColor: '#57454B',
+        borderRadius: 40,
+        opacity: 1,
+    },
+    tc: {
+        backgroundColor: 'black',
+        width: '100%',
+        height: '30%',
+        bottom: 0,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+
+    text: {
+        color: 'white',
     },
 });
 
-export default (Purchases);
+export default Purchases;
