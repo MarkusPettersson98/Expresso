157b3d805a1edb889a87f6316e5a25b46688828f
RobertZetterlund <31474146+RobertZetterlund@users.noreply.github.com>
2019-05-13 11:20:44 +0200

    now fetches picture if it has not been given one to show in Cafe

--- App/pages/components/cafe/Cafe.js
+++ App/pages/components/cafe/Cafe.js
@@ -5,7 +5,10 @@ import CoffeeList from './CoffeeList';
 import CafeHeader from './CafeHeader';
 
 import CartField from './../CartField';
-import { getAllCoffeeFromAShop } from '../../../API/expressoAPI';
+import {
+    getAllCoffeeFromAShop,
+    getShopPicture,
+} from '../../../API/expressoAPI';
 
 /**
  * @file This is the order page entry point. When a user selects a shop they are sent here.
@@ -24,30 +27,36 @@ class Cafe extends React.Component {
         this.state = {
             shop: props.navigation.state.params.selectedShop,
             CoffeItems: [],
+            shopPicture: props.navigation.state.params.picture,
         };
     }
-    
-    static navigationOptions = ({navigation}) => {
-        return {
-        title: navigation.state.params.selectedShop,
-      };
-    }
 
+    static navigationOptions = ({ navigation }) => {
+        return {
+            title: navigation.state.params.selectedShop,
+        };
+    };
 
     async componentDidMount() {
         const allCoffees = await getAllCoffeeFromAShop(this.state.shop);
+        // Did component get passed a picture?  if so, use that, else, fetch that picture.
+        const picture = this.props.navigation.state.params.picture
+            ? this.props.navigation.state.params.picture
+            : await getShopPicture(this.state.shop);
 
-        Promise.all(allCoffees).then(CoffeeList => {
-            this.setState({ ...this.state, CoffeItems: CoffeeList });
+        Promise.all([allCoffees, picture]).then(([coffees, picture]) => {
+            this.setState({
+                ...this.state,
+                CoffeItems: coffees,
+                shopPicture: picture,
+            });
         });
     }
 
     render() {
         return (
             <View style={{ flex: 1 }}>
-                <CafeHeader
-                    picture={this.props.navigation.state.params.picture}
-                />
+                <CafeHeader picture={this.state.shopPicture} />
                 <CoffeeList coffeeItems={this.state.CoffeItems} />
                 <View style={{ marginBottom: 30 }}>
                     <CartField />
