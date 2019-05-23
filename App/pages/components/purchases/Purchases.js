afdef00255584b5da7fc66562e7a3afd3d5de0cd
Markus Pettersson <markus.pettersson1998@gmail.com>
2019-05-23 10:15:54 +0200

    Refactored Purchases into QRPage, for reusabillity and readabillity sake. Also, functional components :-D*

--- App/pages/components/purchases/Purchases.js
+++ App/pages/components/purchases/Purchases.js
@@ -1,11 +1,8 @@
-import React, { Component } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
-import { connect } from 'react-redux';
-import { getReceipt, getScanReceiptLink } from '../../../API/expressoAPI';
-import QRCode from 'react-native-qrcode';
+import React from 'react';
+import { getReceipt } from '../../../API/expressoAPI';
 import NoQRPage from './NoQRPage';
 
-import ReceiptView from './ReceiptView';
+import QRPage from './QRPage';
 
 /**
  * Här ser jag att vi 'kör igång' vår page och hämtar in objektet från backend för att sedan styra komponenterna.
@@ -15,75 +12,31 @@ import ReceiptView from './ReceiptView';
 
 export default class Purchases extends React.Component {
     constructor(props) {
-        console.log('PARAMSUTOMORDERID', props.navigation.state)
+        console.log('Purchases: ', props.navigation.state);
         super(props);
         this.state = {
-            ReceiptView: [],
+            receipt: {},
             receiptId: props.navigation.state.params
                 ? props.navigation.state.params.orderID
-                : false,
+                : '',
         };
     }
 
     async componentDidMount() {
         const receiptId = this.state.receiptId;
 
-        // const receipt = await getReceipt('-LfYMIYIpnqfHjs3Lxdw'); //TODO: Get receipt id dynamically
-        if (receiptId) {
-            const receipt = await getReceipt(receiptId); //TODO: Get receipt id dynamically
-            const receiptView = <ReceiptView receipt={receipt} />;
-        } else {
-            const receiptView = <NoQRPage />;
-        }
-
+        // Check if receipt id exists. If so, resolve receipt
+        const receipt = await getReceipt(receiptId);
         this.setState({
-            ReceiptView: receiptView,
+            receipt: receipt,
         });
     }
 
     render() {
-        return (
-            <View style={styles.container}>
-                <View style={styles.innerContainer}>
-                    <View style={styles.qr}>
-                        <QRCode
-                            value={getScanReceiptLink(this.state.receiptId)} //Ska vara en länk till den beställning som man gjort
-                            size={260}
-                            bgColor="black"
-                            fgColor="white"
-                        />
-                    </View>
-                </View>
-                {this.state.ReceiptView}
-            </View>
+        return this.state.receipt ? (
+            <QRPage receipt={this.state.receipt} />
+        ) : (
+            <NoQRPage />
         );
     }
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        backgroundColor: '#57454B',
-    },
-    innerContainer: {
-        justifyContent: 'center',
-        alignItems: 'center',
-        width: 320,
-        height: 320,
-        backgroundColor: 'white',
-        borderRadius: 40,
-        top: 20,
-    },
-    qr: {
-        width: 300,
-        height: 300,
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderWidth: 5,
-        borderColor: '#57454B',
-        borderRadius: 40,
-        opacity: 1,
-    },
-});
---
+++ App/pages/components/purchases/QRPage.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { View } from 'react-native';
+import ReceiptView from './ReceiptView';
+import { View, StyleSheet } from 'react-native';
+import QRCode from 'react-native-qrcode';
+
+import { getScanReceiptLink } from '../../../API/expressoAPI';
+
+/**
+ * This view shows the emptycart-icon and text, perhaps this view could benefit from recommending the customer a purchase
+ * that s/he has done before.
+ * This view does not have navigation, the navigation lies within the header folder.
+ */
+const QRPage = ({ receipt }) => {
+    return (
+        <View style={styles.container}>
+            <View style={styles.innerContainer}>
+                <View style={styles.qr}>
+                    <QRCode
+                        value={getScanReceiptLink(receipt.id)} //Ska vara en länk till den beställning som man gjort
+                        size={260}
+                        bgColor="black"
+                        fgColor="white"
+                    />
+                </View>
+            </View>
+            <ReceiptView />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        backgroundColor: '#57454B',
+    },
+    innerContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: 320,
+        height: 320,
+        backgroundColor: 'white',
+        borderRadius: 40,
+        top: 20,
+    },
+    qr: {
+        width: 300,
+        height: 300,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderWidth: 5,
+        borderColor: '#57454B',
+        borderRadius: 40,
+        opacity: 1,
+    },
+});
+
+export default QRPage;
