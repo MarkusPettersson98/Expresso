e1bdd082987d2ca206370c32f275435ac24f8fe1
Emil Svensson <esvensson1997@gmail.com>
2019-05-23 10:36:11 +0200

    snabb-fix as error på flik köp

--- App/pages/OrderPage.js
+++ App/pages/OrderPage.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
+import { Text } from 'react-native';
 import Purchases from './components/purchases/Purchases';
 
 export default class orderpage extends Component {
     render() {
-        return <Purchases QR={this.props.navigation.state.params.orderID} />;
+        if (this.props.navigation.state.params){
+            return <Purchases QR={this.props.navigation.state.params.orderID} />;
+        }
+        else{
+           return <Text> Make a purchase </Text>;
+        }
     }
 }
