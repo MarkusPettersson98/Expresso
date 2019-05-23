6bd7eae325a28ed6848e93aaf7fda3429546d653
Emil Svensson <esvensson1997@gmail.com>
2019-05-23 20:58:52 +0200

    Merged
    (Se över placeringar i botContainer)

--- App/pages/components/purchases/ReceiptView.js
+++ App/pages/components/purchases/ReceiptView.js
@@ -204,7 +204,7 @@ const styles = StyleSheet.create({
         height: 50,
         backgroundColor: '#FAFAFA',
         top: 40,
-    },
+    }, 
     botContainer: {
         width: '100%',
         height: '100%',
