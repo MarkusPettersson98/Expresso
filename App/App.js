73a02242e3b87efcedd8ba86fdc72d7da50a116c
Iman Radjavi <radjavi@hotmail.com>
2019-05-25 17:20:33 +0200

    preload fonts (icons)

--- App/App.js
+++ App/App.js
@@ -12,12 +12,51 @@ import expressoApp from './pages/components/redux/reducers';
 import * as firebase from 'firebase/app';
 import firebaseConfig from './firebaseConfig';
 
+import { AppLoading, Asset, Font } from 'expo';
+import {
+  SimpleLineIcons,
+  AntDesign,
+  Ionicons,
+  Feather,
+  MaterialIcons,
+} from '@expo/vector-icons';
+
 firebase.initializeApp(firebaseConfig);
 
 const store = createStore(expressoApp);
 
+function cacheFonts(fonts) {
+  return fonts.map(font => Font.loadAsync(font));
+}
+
 export default class App extends React.Component {
+  state = {
+    isReady: false,
+  };
+
+  async _loadAssetsAsync() {
+    const fontAssets = cacheFonts([
+      SimpleLineIcons.font,
+      AntDesign.font,
+      Ionicons.font,
+      Feather.font,
+      MaterialIcons.font,
+    ]);
+
+    await Promise.all([...fontAssets]);
+  }
+
   render() {
+    if (!this.state.isReady) {
+      return (
+        <AppLoading
+          startAsync={this._loadAssetsAsync}
+          onFinish={() => this.setState({ isReady: true })}
+          onError={console.warn}
+        />
+      );
+    }
+
     return (
       <Provider store={store}>
         <View style={{ flex: 1 }}>
